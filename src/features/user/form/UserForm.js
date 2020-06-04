dd9d052aac81fb52fb82ca18c52d76290cd9dc48
kyoung90 <kenneth.young90@gmail.com>
2020-06-03 23:38:23 -0400

    change controlled component to uncontroller component
    Co-authored-by: Vinh Do <vinhdo07@gmail.com>

--- src/features/user/form/UserForm.js
+++ src/features/user/form/UserForm.js
@@ -8,126 +8,97 @@ import centers from "../../../commons/centers";
 import { SelectInput } from "../../../commons/inputs/SelectInput";
 import Error from "../../../commons/inputs/Error";
 
-import {
-  FormControl, 
-  MenuItem, 
-  Select, 
-  TextField
-} from "@material-ui/core";
+import { FormControl, MenuItem, Select, TextField } from "@material-ui/core";
 
 import { useForm, Controller } from "react-hook-form";
 import "./userForm.css";
 
 export default function UserForm(props) {
-  const { control, errors, handleSubmit } = useForm();
+  const { control, errors, handleSubmit, watch } = useForm();
 
-  const [formData, setFormData] = useState({
-    first_name: { type: "text", value: "", required: true, label: "Nombre" },
+  const accountType = watch("account_type");
+
+  const formData = {
+    first_name: { type: "text", required: true, label: "Nombre" },
     second_name: {
       type: "text",
-      value: "",
+
       required: false,
       label: "Segundo nombre",
     },
     paternal_surname: {
       type: "text",
-      value: "",
+
       required: true,
       label: "Apellido 1",
     },
     maternal_surname: {
       type: "text",
-      value: "",
+
       required: false,
       label: "Apellido 2",
     },
     email: {
       type: "email",
-      value: "",
+
       required: true,
       label: "Correo electrónico",
     },
-    phone_number: { type: "tel", value: "", required: true, label: "Teléfono" },
+    phone_number: { type: "tel", required: true, label: "Teléfono" },
     password: {
       type: "password",
-      value: "",
+
       required: true,
       label: "Contraseña",
     },
     password_confirmation: {
       type: "password",
-      value: "",
+
       required: true,
       label: "Confirmar contraseña",
     },
     center: {
       type: "select",
-      value: centers[0],
+
       required: true,
       label: "Centro",
       items: centers,
     },
     residence_municipality: {
       type: "text",
-      value: "",
+
       required: true,
       label: "Municipio",
     },
     gender: {
       type: "select",
-      value: genders[0],
+
       required: true,
       label: "Género",
       items: genders,
     },
     account_type: {
       type: "select",
-      value: titles[0],
+
       required: true,
       label: "Título",
       items: titles,
     },
-  });
+  };
 
   useEffect(() => {
-    if (formData.account_type.value === "student") {
-      setFormData(
-        ...formData,
-        (grades = {
-          type: "select",
-          value: grades[0],
-          required: true,
-          label: "Grado",
-          items: grades,
-        })
-      );
+    if (accountType === "student") {
+      formData.grades = {
+        type: "select",
+        required: true,
+        label: "Grado",
+        items: grades,
+      };
+      console.log(formData)
     } else {
-      setFormData({ ...formData, grades: undefined });
+      delete formData.grades;
     }
-  }, [formData.account_type]);
-
-  const handleChange = (e) => {
-    const event = e[0];
-    setFormData({
-      ...formData,
-      [event.target.name]: {
-        ...formData[event.target.name],
-        value: event.target.value,
-      },
-    });
-  };
-
-  const handleSelectChange = (event) => {
-    debugger;
-    console.log(event.target.name, event.target.value);
-    setFormData({
-      ...formData,
-      [event.target.name]: {
-        ...formData[event.target.name],
-        value: event.target.value,
-      },
-    });
-  };
+  }, [watch("account_type")]);
 
   const onSubmit = (data, e) => {
     console.log("Submit event", e);
@@ -149,13 +120,12 @@ export default function UserForm(props) {
                   <TextField
                     id="student-name"
                     label={formData[field].label}
-                    value={formData[field].value}
                     variant="outlined"
                     className={"dark-purple-text textfield-outlined"}
                     error={errors[field]}
                   />
                 }
-                onChange={handleChange}
+                // onChange={handleChange}
                 name={field}
                 control={control}
                 rules={{ required: formData[field].required }}
@@ -176,10 +146,9 @@ export default function UserForm(props) {
             name={field}
             label={formData[field].label}
             invert={true}
-            value={formData[field].value}
             labelWidth={70}
             items={formData[field].items}
-            handleChange={handleSelectChange}
+            // handleChange={handleSelectChange}
             control={control}
             errors={errors[field]}
           />
@@ -192,14 +161,13 @@ export default function UserForm(props) {
                 <TextField
                   id="student-name"
                   label={formData[field].label}
-                  value={formData[field].value}
                   variant="outlined"
                   className={"dark-purple-text textfield-outlined"}
                   error={errors[field]}
                   type={formData[field].type}
                 />
               }
-              onChange={handleChange}
+              // onChange={handleChange}
               name={field}
               control={control}
               rules={{ required: formData[field].required }}
