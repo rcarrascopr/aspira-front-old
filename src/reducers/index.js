ca23f6c805f3ff340d92d52f82bcb85ac8da4a08
bebopvinh <vinhdo07@gmail.com>
2020-06-11 02:26:02 -0700

    add productsReducer to combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import usersReducer from "./usersReducer";
 import centersReducer from "./centersReducer";
 import utisReducer from "./utisReducer";
+import productsReducer from "./productsReducer";
 
 const rootReducer = combineReducers({
   users: usersReducer,
   centers: centersReducer,
   utis: utisReducer,
+  products: productsReducer,
 });
 
 export default rootReducer;
