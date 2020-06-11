e8cf4c94e8ed5d69775559304bae1a493db069b1
bebopvinh <vinhdo07@gmail.com>
2020-06-11 02:26:34 -0700

    add cases for LOADING and SET_PRODUCT

---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "LOADING":
+      return { ...state, loading: true };
+    case "SET_PRODUCT":
+      return { ...state, product: payload, loading: false };
+
+    default:
+      return state;
+  }
+};
