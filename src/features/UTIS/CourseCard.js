f103d4825c751356acdf919a9b50ea47660f43e3
bebopvinh <vinhdo07@gmail.com>
2020-06-12 15:08:56 -0700

    change props to render students amount
    - backend object will have a total_students attribute

--- src/features/UTIS/CourseCard.js
+++ src/features/UTIS/CourseCard.js
@@ -13,9 +13,7 @@ const CourseCard = (props) => {
       <div className="course-card-content">
         <h2 className="dark-purple-text">{props.name}</h2>
         <div>
-          <p className="dark-purple-text">
-            {props.students.length} Estudiantes
-          </p>
+          <p className="dark-purple-text">{props.total_students} Estudiantes</p>
           <p className="dark-purple-text">{props.products}6 Productos</p>
         </div>
       </div>
