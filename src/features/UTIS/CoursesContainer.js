8390334be7f8937cd253b80321a8bd706001f98d
bebopvinh <vinhdo07@gmail.com>
2020-06-12 15:10:10 -0700

    change default state, add conditional
    - conditional will render Cards once courses are set

--- src/features/UTIS/CoursesContainer.js
+++ src/features/UTIS/CoursesContainer.js
@@ -6,25 +6,30 @@ import { Pagination } from "../../commons/pagination/Pagination";
 import { withRouter } from "react-router-dom";
 
 const CoursesContainer = (props) => {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState("");
 
   useEffect(() => {
     setCourses(props.courses);
   }, [props.courses]);
 
   const handleClick = () => {
-      props.history.push("/utis/create")
-  }
+    props.history.push("/utis/create");
+  };
 
   const generateCards = () => {
-    return courses.slice(0, 15).map((course) => <CourseCard {...course} />);
+    if (!!courses) {
+      return courses.slice(0, 15).map((course) => <CourseCard {...course} />);
+    }
   };
   return (
     <section className="courses-container">
       <h1>Unidades Temáticas Integradas (UTIS)</h1>
 
       <div className="card-group">
-        <div className="course-card dark-purple white-text" onClick={handleClick}>
+        <div
+          className="course-card dark-purple white-text"
+          onClick={handleClick}
+        >
           <div className="course-card-create">
             <p className="plus-icon">+</p>
             <h2>Crear nueva UTIS</h2>
