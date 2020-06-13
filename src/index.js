16edca07442cc9b5ce42aa070872306fa9813622
bebopvinh <vinhdo07@gmail.com>
2020-06-12 20:44:45 -0700

    add config for chrome redux devtools

--- src/index.js
+++ src/index.js
@@ -8,9 +8,16 @@ import rootReducer from "./reducers/index";
 import thunk from "redux-thunk";
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
