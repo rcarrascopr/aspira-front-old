e3910a3fcf4933a7e13f7f12cb5ee5528a1b1b18
bebopvinh <vinhdo07@gmail.com>
2020-06-14 17:07:16 -0700

    update form to persist Data (see details)
    - destructure props
    - mapDispatchToProps has more actions
    - actions will fetch centers, semesters, and teachers
    - map fetched data to props from global store
    - map utisFormData to props for form persistence
    - add buttons to step through multiple sub forms
    - add function to generate buttons per step
    
    Co-authored-by: Kenneth Young Castro <kenneth.young90@gmail.com>

--- src/features/UTIS/form/UTISFormContainer2.js
+++ src/features/UTIS/form/UTISFormContainer2.js
@@ -6,49 +6,80 @@ import { Details } from "./Details";
 import { Students } from "./Students";
 
 import { fetchCenters } from "../../../actions/centerActions";
+import { fetchTeachers } from "../../../actions/userActions";
+import { fetchSemesters } from "../../../actions/semesterActions";
+import { setUTISFormData } from "../../../actions/utisActions";
 
 import "./UTISForm.css";
+import "../../products/form/ProductFormContainer.css";
 
-export const UTISFormContainer2 = ({ centers }) => {
-  const { handleSubmit } = useForm();
+export const UTISFormContainer2 = (props) => {
+  const {
+    centers,
+    semesters,
+    teachers,
+    utisFormData,
+    fetchCenters,
+    fetchSemesters,
+    fetchTeachers,
+  } = props;
+
+  const { control, errors, reset, handleSubmit } = useForm({
+    defaultValues: utisFormData,
+  });
   const [currentStep, setCurrentStep] = useState(0);
-  let initialState = {
-    utisName: "",
-    studentName: "",
-  };
-  const [formData, setFormData] = useState(initialState);
 
   //fetch centers after mounted
   useEffect(() => {
     fetchCenters();
-  }, [centers]);
+    fetchSemesters();
+    fetchTeachers();
+  }, [fetchCenters, fetchSemesters, fetchTeachers]);
+
+  console.log("Props from UTIS Form: ", props);
 
   const onSubmit = (data, event) => {
     console.log("data form UTIS form: ", data);
   };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-    console.log("Current formData: ", formData);
-  };
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setFormData((prevState) => ({
+  //     ...prevState,
+  //     [name]: value,
+  //   }));
+  //   console.log("Current formData: ", formData);
+  // };
 
-  const renderCurrentStep = () => {
-    const childrenProps = { centers, handleChange, formData };
+  const generateForm = () => {
+    const childrenProps = { control, errors, reset };
+    const detailsProps = { centers, semesters, teachers };
     switch (currentStep) {
       case 1:
-        return <Students {...childrenProps} />;
+        return <Students />;
       default:
-        return <Details {...childrenProps} />;
+        return <Details {...childrenProps} {...detailsProps} />;
     }
   };
-  const handleClick = (event) => {
-    event.preventDefault();
-    const name = event.target.name;
-    console.log(name);
+
+  const tabs = ["Detalles", "Estudiantes"];
+
+  const generateTabs = () => {
+    return tabs.map((tab, index) => (
+      <p
+        key={index}
+        className={`dark-purple-text ${
+          index === currentStep ? "active-tab" : ""
+        }`}
+      >
+        {tab}
+      </p>
+    ));
+  };
+
+  /* Code for button navigation and keeping track of current step in the form process */
+  const handleClick = (name) => {
+    console.log("button name: ", name);
     if (name === "previous" && currentStep > 0) {
       setCurrentStep(currentStep - 1);
     } else if (name === "next" && currentStep <= 0) {
@@ -56,37 +87,63 @@ export const UTISFormContainer2 = ({ centers }) => {
     }
   };
 
+  const generateButtons = () => {
+    const hidden = currentStep === 0 ? "hidden" : "";
+    return (
+      <>
+        <a
+          className={`primary-btn ${hidden}`}
+          onClick={() => handleClick("previous")}
+        >
+          Atrás
+        </a>
+
+        {currentStep !== 1 && (
+          <a className="primary-btn" onClick={() => handleClick("next")}>
+            Próximo
+          </a>
+        )}
+        {currentStep === 1 && (
+          <input type="submit" className="primary-btn" value="Guardar" />
+        )}
+      </>
+    );
+  };
+
+  /* End of button navigation */
+
   return (
-    <form className="utis-form-container courses-purple">
+    <form
+      className="utis-form-container courses-purple"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <Paper className="form-container" elevation={3}>
-        {renderCurrentStep()}
+        {generateForm()}
       </Paper>
       <div className="utis-details-options">
         <div className="side-tabs">
           <p>Current Step: {currentStep}</p>
-          <p className="details">Details</p>
-          <p className="students">Estudiantes</p>
+
+          {generateTabs()}
         </div>
-        <div className="button-container">
-          <button name="previous" onClick={handleClick}>
-            Previous
-          </button>
-          <button name="next" onClick={handleClick}>
-            Next
-          </button>
-        </div>
-        <button type="submit" onClick={handleSubmit(onSubmit)}>
-          Submit
-        </button>
+        <div className="utis-details-button-group"> {generateButtons()}</div>
       </div>
     </form>
   );
 };
 
-const mapStateToProps = (state) => ({ centers: state.centers.centers });
+const mapStateToProps = (state) => ({
+  centers: state.centers.centers,
+  semesters: state.semesters.semesters,
+  teachers: state.users.teachers,
+  utisFormData: state.utis.utisFormData,
+});
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCenters: dispatch(fetchCenters()),
+  fetchCenters: () => dispatch(fetchCenters()),
+  fetchSemesters: () => dispatch(fetchSemesters()),
+  fetchTeachers: () => dispatch(fetchTeachers()),
+  setUTISFormData: (data) => dispatch(setUTISFormData(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UTISFormContainer2);
