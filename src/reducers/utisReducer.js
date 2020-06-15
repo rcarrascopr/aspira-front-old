8508a8f5960e77b9dc58db384e417f5e1c981019
bebopvinh <vinhdo07@gmail.com>
2020-06-14 17:03:54 -0700

    add defaultValues in utisFormData

--- src/reducers/utisReducer.js
+++ src/reducers/utisReducer.js
@@ -1,5 +1,17 @@
 export default function utisReducer(
-  state = { loading: false, courses: [] },
+  state = {
+    loading: false,
+    courses: [],
+    utisFormData: {
+      name: "",
+      category: "",
+      semester_id: "",
+      center_id: "",
+      teacher_id: "",
+      grade: "",
+      students: [""],
+    },
+  },
   action
 ) {
   switch (action.type) {
@@ -7,6 +19,9 @@ export default function utisReducer(
       return { ...state, loading: true };
     case "FETCH_COURSES":
       return { ...state, courses: [...action.payload], loading: false };
+    case "SET_UTIS_FORM_DATA":
+      debugger;
+      return { ...state, utisFormData: action.payload };
     default:
       return state;
   }
