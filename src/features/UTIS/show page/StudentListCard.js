b781c6ad3509cc2c9bd47c1941a60b6c5f529c50
bebopvinh <vinhdo07@gmail.com>
2020-06-15 03:44:17 -0700

    add useContext and conditional (details)
    - useContext to retrieve course from top level
    - feed students from course to PersonListContainer

--- src/features/UTIS/show page/StudentListCard.js
+++ src/features/UTIS/show page/StudentListCard.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import fake_students from "../../../commons/fake_students";
 
 import { PersonListContainer } from "../../../commons/person list/PersonListContainer";
+import CourseContext from "../../../contexts/CourseContext";
 
 export default function StudentListCard() {
+  const course = useContext(CourseContext);
   return (
     <div className="card">
       <h3 className="dark-purple-text">Estudiantes en el curso</h3>
-      <PersonListContainer items={fake_students} />
+      {!!course && <PersonListContainer items={course.students} />}
     </div>
   );
 }
