539f3427ae5bbe4da3480f9d0fc08c0722b916e0
bebopvinh <vinhdo07@gmail.com>
2020-07-05 00:42:49 -0700

    add conditional to render error messages
    - if error messages exist from props, they will be rendered

--- src/commons/inputs/Error.js
+++ src/commons/inputs/Error.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 export default function Error(props) {
+  const renderMessage = () => {
+    if (!!props.errors) {
+      return props.errors.message;
+    } else {
+      return "Esto es requerido.";
+    }
+  };
+
   return (
     <>
       {props.errors && (
         <p className="tooltip">
-          ⚠️ <span className="tooltiptext">Esto es requerido.</span>
+          ⚠️ <span className="tooltiptext">{renderMessage()}</span>
         </p>
       )}
     </>
