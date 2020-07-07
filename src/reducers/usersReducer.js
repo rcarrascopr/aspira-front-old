c0819e95f2f8f68f99d676301c2b947edd7a90fb
bebopvinh <vinhdo07@gmail.com>
2020-07-07 01:02:47 -0700

    extract default state into `const initialState`
    - add `success` key to render after successful account update
    - "LOGOUT_USER" will return initialState and make currentUser null

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,29 +1,29 @@
-export default function usersReducer(
-  state = {
-    loading: false,
-    error: "",
-    currentUser: JSON.parse(localStorage.getItem("currentUser")),
-    fetchedUser: "",
-    teachers: [],
-    defaultValues: {
-      id: "",
-      email: "",
-      created_at: "",
-      updated_at: "",
-      first_name: "",
-      second_name: "",
-      maternal_surname: "",
-      paternal_surname: "",
-      center_id: 1,
-      academic_level: "9",
-      badge_id: "",
-      gender: "",
-      phone_number: "",
-      residence_municipality: "",
-    },
+const initialState = {
+  loading: false,
+  error: "",
+  success: "",
+  currentUser: JSON.parse(localStorage.getItem("currentUser")),
+  fetchedUser: "",
+  teachers: [],
+  defaultValues: {
+    id: "",
+    email: "",
+    created_at: "",
+    updated_at: "",
+    first_name: "",
+    second_name: "",
+    maternal_surname: "",
+    paternal_surname: "",
+    center_id: 1,
+    academic_level: "9",
+    badge_id: "",
+    gender: "",
+    phone_number: "",
+    residence_municipality: "",
   },
-  action
-) {
+};
+
+export default function usersReducer(state = initialState, action) {
   switch (action.type) {
     case "LOADING_USER":
       return { ...state, loading: true, fetchedUser: "", error: "" };
@@ -34,7 +34,7 @@ export default function usersReducer(
     case "LOGOUT_USER":
       localStorage.removeItem("token");
       localStorage.removeItem("currentUser");
-      return { loading: false, currentUser: null };
+      return { ...initialState, currentUser: null };
     // case "SIGNUP":
     //   return { ...state, loading: false };
     case "CREATE_USER":
@@ -57,6 +57,8 @@ export default function usersReducer(
       return { ...state, error: action.payload, loading: false };
     case "CLEAR_USER_ERRORS":
       return { ...state, error: "" };
+    case "SET_USER_SUCCESS":
+      return { ...state, success: action.payload, error: "", loading: false };
     default:
       return state;
   }
