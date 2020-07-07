60fea2e75dbbb49c3749990cfa90fe05df848589
bebopvinh <vinhdo07@gmail.com>
2020-07-07 15:47:44 -0700

    add renderWithCustomProps()
    - will add custom props to a rendered component if provided

--- src/commons/PrivateRoute.js
+++ src/commons/PrivateRoute.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-export const PrivateRoute = ({ component: Component, ...rest }) => {
+export const PrivateRoute = ({
+  component: Component,
+  customProps,
+  ...rest
+}) => {
   const isLoggedIn = localStorage.getItem("token");
+  const renderWithCustomProps = (props) => {
+    return customProps ? (
+      <Component {...props} {...customProps} />
+    ) : (
+      <Component {...props} />
+    );
+  };
 
   return (
     <Route
       {...rest}
-      render={props =>
+      render={(props) =>
         isLoggedIn ? (
-          <Component {...props} />
+          // <Component {...props} />
+          renderWithCustomProps(props)
         ) : (
           <>
             <Redirect
