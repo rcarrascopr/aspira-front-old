eab6edb7ec38cdfdb8c824928bbdad6683d7ed0e
kyoung90 <kenneth.young90@gmail.com>
2020-07-08 11:31:21 -0400

    -Now confirms if paternal or maternal surname is empty before displaying

--- src/commons/person list/PersonListItem.js
+++ src/commons/person list/PersonListItem.js
@@ -3,13 +3,21 @@ import { NameCircle } from "../NameCircle";
 
 export const PersonListItem = (props) => {
   return (
-    <li key={props.item.id} className="list-item" onClick={(event) => props.handleClick(event, props.item)}>
+    <li
+      key={props.item.id}
+      className="list-item"
+      onClick={(event) => props.handleClick(event, props.item)}
+    >
       <div className="list-item-content">
         <NameCircle
           size="small"
           initials={props.item.first_name[0] + props.item.paternal_surname[0]}
         />
-        <p className="dark-purple-text">{`${props.item.first_name} ${props.item.paternal_surname} ${props.item.maternal_surname}`}</p>
+        <p className="dark-purple-text">{`${props.item.first_name} ${
+          props.item.paternal_surname ? props.item.paternal_surname : ""
+        } ${
+          props.item.maternal_surname ? props.item.maternal_surname : ""
+        }`}</p>
       </div>
     </li>
   );
