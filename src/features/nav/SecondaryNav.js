8305bb514777d42dd60f7eb1ca60e0bb3911c94c
kyoung90 <kenneth.young90@gmail.com>
2020-07-08 16:15:39 -0400

    -Navbar now fetches semesters from the backend

--- src/features/nav/SecondaryNav.js
+++ src/features/nav/SecondaryNav.js
@@ -1,26 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { NameCircle } from "../../commons/NameCircle";
-
 import SelectInput from "../../commons/inputs/SelectInput";
 
+import { fetchSemesters } from "../../actions/semesterActions";
+
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-export const SecondaryNav = () => {
-  const [semester, setSemester] = useState("2015-16 01");
+const SecondaryNav = (props) => {
+  // const [semester, setSemester] = useState("2015-16 01");
 
-  const handleChange = (event) => {
-    setSemester(event.target.value);
-  };
+  // const handleChange = (event) => {
+  //   setSemester(event.target.value);
+  // };
+
+  useEffect(() => {
+    props.fetchSemesters();
+  }, []);
 
   return (
     <section className="secondary-nav very-light-purple white-text">
       <div className="nav-header">
         {/* <img src="" alt="Profile image" /> */}
-        <NameCircle initials="KY" />
+        <NameCircle
+          initials={`${props.currentUser.name[0]}${
+            props.currentUser.name.split(" ")[1][0]
+          }`}
+        />
 
         <div className="name-header">
-          <h2>Nombre de usuario</h2>
+          <h2>{props.currentUser.name}</h2>
           <Link to="/" className="white-text">
             Ver mi perfil
           </Link>
@@ -30,16 +40,26 @@ export const SecondaryNav = () => {
       <SelectInput
         name="semester"
         label="Semestre"
-        value={semester}
+        // value={semester}
         labelWidth={70}
-        items={[
-          { name: "2015-16 01", id: 1 },
-          { name: "2015-16 02", id: 2 },
-        ]}
-        handleChange={handleChange}
+        items={props.semesters}
+        // handleChange={handleChange}
       />
     </section>
   );
 };
 
-export default SecondaryNav;
+let mapStateToProps = (state) => {
+  return {
+    currentUser: state.users.currentUser,
+    semesters: state.semesters.semesters,
+  };
+};
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    fetchSemesters: () => dispatch(fetchSemesters()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SecondaryNav);
