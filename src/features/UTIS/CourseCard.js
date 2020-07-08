997b914c3c7160a902acb671eb7df0950e85cca4
kyoung90 <kenneth.young90@gmail.com>
2020-07-08 13:58:18 -0400

    -Add duplicate and edit icons -Removed onclick on coursecard and added it to the name instead

--- src/features/UTIS/CourseCard.js
+++ src/features/UTIS/CourseCard.js
@@ -7,14 +7,42 @@ const CourseCard = (props) => {
   const handleClick = (id) => {
     props.history.push(`/utis/${id}`);
   };
+
+  const handleEditClick = (id) => {
+    props.history.push(`/utis/${id}/edit`);
+  };
   return (
-    <div className="course-card white" onClick={() => handleClick(props.id)}>
+    <div className="course-card white">
       <div className={`course-card-header ${cardColors[props.category]}`} />
       <div className="course-card-content">
-        <h2 className="dark-purple-text">{props.name}</h2>
+        <h2 className="dark-purple-text pointer" onClick={() => handleClick(props.id)}>
+          {props.name}
+        </h2>
         <div>
           <p className="dark-purple-text">{props.total_students} Estudiantes</p>
           <p className="dark-purple-text">{props.total_products} Productos</p>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              width: "100%",
+              alignItems: "center",
+            }}
+          >
+            <img
+              className="icon pointer"
+              src="/assets/duplicate_icon.png"
+              alt="duplicar"
+              style={{ padding: "0px 10px" }}
+            />
+            <img
+              className="icon pointer"
+              src="/assets/edit_icon.png"
+              alt="editar"
+              onClick={() => handleEditClick(props.id)}
+            />
+          </div>
         </div>
       </div>
     </div>
