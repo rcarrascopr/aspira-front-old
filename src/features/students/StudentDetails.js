09de06ff0bbac8f067efe5f443aba3bea9cc5f13
kyoung90 <kenneth.young90@gmail.com>
2020-07-08 16:33:28 -0400

    -Add student details header and courses

--- src/features/students/StudentDetails.js
+++ src/features/students/StudentDetails.js
@@ -2,11 +2,22 @@ import React from "react";
 
 import { NameCircle } from "../../commons/NameCircle";
 
+import CoursesListContainer from "../../commons/courses list/CoursesListContainer";
+
 export default function StudentDetails(props) {
   if (!(props.currentStudent && props.currentStudent.id)) {
-    return <div className="white student-details-container" style={{display: "flex", 'justifyContent': "center", 'alignItems': 'center'}}>
-      <h2 style={{color: '#AAA'}}>Selecciona un estudiante</h2>
-    </div>
+    return (
+      <div
+        className="white student-details-container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2 style={{ color: "#AAA" }}>Selecciona un estudiante</h2>
+      </div>
+    );
   }
 
   return (
@@ -21,13 +32,18 @@ export default function StudentDetails(props) {
         />
         <ul>
           <p>{`${props.currentStudent.first_name} ${props.currentStudent.paternal_surname} ${props.currentStudent.maternal_surname}`}</p>
-        <li>{props.currentStudent.email}</li>
+          <li>{props.currentStudent.email}</li>
           <li>{props.currentStudent.phone_number}</li>
-        <li>{props.currentStudent.badge_id}</li>
-        <li>Grade: {props.currentStudent.academic_level}</li>
+          <li>{props.currentStudent.badge_id}</li>
+          <li>Grade: {props.currentStudent.academic_level}</li>
         </ul>
       </div>
-      <div className="student-courses-details"></div>
+      <div className="student-courses-details">
+        <h3 className="dark-purple-text student-details-header">
+          UTI que está tomando
+        </h3>
+        <CoursesListContainer items={props.currentStudent.courses} />
+      </div>
       <div className="student-details-buttons dark-purple-text">
         <a className="lined-button">Generar Reporte</a>
         <a className="lined-button">Editar</a>
