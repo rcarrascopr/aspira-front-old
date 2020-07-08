c9131c386d5598153311f24bd2ec75b9007d316b
kyoung90 <kenneth.young90@gmail.com>
2020-07-08 16:34:36 -0400

    -Add props to courses list container -Add links to course list item

--- src/commons/courses list/CoursesListContainer.js
+++ src/commons/courses list/CoursesListContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import CoursesListItem from "./CoursesListItem";
 
-export default function CoursesListContainer() {
+export default function CoursesListContainer(props) {
   const generateItems = () => {
     return props.items.map((item) => <CoursesListItem item={item} />);
   };
--- src/commons/courses list/CoursesListItem.js
+++ src/commons/courses list/CoursesListItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import {Link} from "react-router-dom"
+
 export default function CoursesListItem(props) {
   return (
     <li key={props.item.id} className="list-item">
       <div className="list-item-content">
-        <a className="dark-purple-text">
+        <Link className="dark-purple-text" to={`/utis/${props.item.id}`} >
           {props.item.name} - {props.item.category}
-        </a>
+        </Link>
       </div>
     </li>
   );
