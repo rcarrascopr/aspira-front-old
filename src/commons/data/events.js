8506a83568e722bef81c2600f4b15085fa2c1eb6
kyoung90 <kenneth.young90@gmail.com>
2020-07-24 13:01:16 -0400

    Change fake events date to match date picker

--- src/commons/data/events.js
+++ src/commons/data/events.js
@@ -1,30 +1,30 @@
 export default [
-    {
-      id: 1,
-      name: "Receso navideño",
-      category: "general",
-      description: "Empezamos la navidad.",
-      date: "27 de Febrero de 2020",
-    },
-    {
-      id: 2,
-      name: "Semana de la puertorriqueñidad",
-      category: "faculty",
-      description: "Celebramos nuestras raices.",
-      date: "27 de Febrero de 2020",
-    },
-    {
-      id: 3,
-      name: "Dia de la amistad",
-      category: "administration",
-      description: "Celebramos la amistad. Feriado.",
-      date: "27 de Febrero de 2020",
-    },
-    {
-      id: 4,
-      name: "Semana de acción de gracia",
-      category: "general",
-      description: "Damos las gracias por lo que tenemos.",
-      date: "27 de Febrero de 2020",
-    },
-  ];
+  {
+    id: 1,
+    name: "Receso navideño",
+    category: "general",
+    description: "Empezamos la navidad.",
+    date: "2020-07-21",
+  },
+  {
+    id: 2,
+    name: "Semana de la puertorriqueñidad",
+    category: "faculty",
+    description: "Celebramos nuestras raices.",
+    date: "2020-07-21",
+  },
+  {
+    id: 3,
+    name: "Dia de la amistad",
+    category: "administration",
+    description: "Celebramos la amistad. Feriado.",
+    date: "2020-07-21",
+  },
+  {
+    id: 4,
+    name: "Semana de acción de gracia",
+    category: "general",
+    description: "Damos las gracias por lo que tenemos.",
+    date: "2020-07-21",
+  },
+];
