e2fd6f0f6edf3f688eb4eb405952a56d4f15d118
kyoung90 <kenneth.young90@gmail.com>
2020-07-24 13:01:58 -0400

    Add events show

--- src/features/events/EventsShow.js
+++ src/features/events/EventsShow.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function EventsShow() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function EventsShow(props) {
+  const handleClick = (e) => {
+    e.preventDefault();
+    props.setCardContent("edit");
+  };
+  return (
+    <section className="events-show">
+      <h2>{props.event.date}</h2>
+      <p>{props.event.name}</p>
+      <p>{props.event.description}</p>
+      <p>
+        Categoría: <strong>{props.event.category}</strong>
+      </p>
+      <a className="primary-btn" onClick={handleClick}>
+        Editar Evento
+      </a>
+    </section>
+  );
 }
