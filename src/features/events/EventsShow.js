fffb8befdf2b4055253d47ee96985abeb1bdc70b
kyoung90 <kenneth.young90@gmail.com>
2020-08-02 15:32:05 -0400

    -Change center to be at the bottom of the text shown. Text is now purple

--- src/features/events/EventsShow.js
+++ src/features/events/EventsShow.js
@@ -7,13 +7,14 @@ export default function EventsShow(props) {
   };
   return (
     <section className="events-show">
-      <h2>{props.event.date}</h2>
-      <p>Centro: {props.event.center.name}</p>
-      <p>{props.event.name}</p>
-      <p>{props.event.description}</p>
-      <p>
+      <h2 className="dark-purple-text">{props.event.date}</h2>
+    
+      <p className="dark-purple-text">{props.event.name}</p>
+      <p className="dark-purple-text">{props.event.description}</p>
+      <p className="dark-purple-text">
         Categoría: <strong>{props.event.category}</strong>
       </p>
+      <p className="dark-purple-text">Centro: {props.event.center.name}</p>
       <a className="primary-btn" onClick={handleClick}>
         Editar Evento
       </a>
