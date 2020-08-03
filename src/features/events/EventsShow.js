d4e7b304b29340cf7c09220c86e8355b101bae0a
kyoung90 <kenneth.young90@gmail.com>
2020-08-03 15:47:44 -0400

    -Fix error where selecting a semester, and then changing from school year to events would cause events show page to crash

--- src/features/events/EventsShow.js
+++ src/features/events/EventsShow.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import generateDate from "../../commons/dateParser"
+import generateDate from "../../commons/dateParser";
 
 export default function EventsShow(props) {
   const handleClick = (e) => {
@@ -8,29 +8,39 @@ export default function EventsShow(props) {
     props.setCardContent("edit");
   };
 
-  return (
-    <section className="events-show">
-      <div>
-        <h2 className="dark-purple-text">{generateDate()}</h2>
+  if (props.event && props.event.center) {
+    return (
+      <section className="events-show">
+        <div>
+          <h2 className="dark-purple-text">{generateDate()}</h2>
 
-        <p className="dark-purple-text event-name">{props.event.name}</p>
-      </div>
+          <p className="dark-purple-text event-name">{props.event.name}</p>
+        </div>
 
-      <p className="dark-purple-text event-text">{props.event.description}</p>
-      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <p className="dark-purple-text event-text">
-          Categoría: <strong>{props.event.category}</strong>
-        </p>
-        <p className="dark-purple-text event-text">
-          Centro: {props.event.center.name}
-        </p>
-        <a
-          className="primary-btn-outline dark-purple-text font-weight-normal event-text"
-          onClick={handleClick}
+        <p className="dark-purple-text event-text">{props.event.description}</p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          Editar
-        </a>
-      </div>
-    </section>
-  );
+          <p className="dark-purple-text event-text">
+            Categoría: <strong>{props.event.category}</strong>
+          </p>
+          <p className="dark-purple-text event-text">
+            Centro: {props.event.center.name}
+          </p>
+          <a
+            className="primary-btn-outline dark-purple-text font-weight-normal event-text"
+            onClick={handleClick}
+          >
+            Editar
+          </a>
+        </div>
+      </section>
+    );
+  } else {
+    return <></>;
+  }
 }
