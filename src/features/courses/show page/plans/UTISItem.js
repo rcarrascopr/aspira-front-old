35ae02c829e8c5fcab76f18b6d179d23bbdbcf35
kyoung90 <kenneth.young90@gmail.com>
2020-08-09 16:34:18 -0400

    -Set utis header height -List item now shows total activities instead of products

--- src/features/courses/show page/plans/UTISItem.js
+++ src/features/courses/show page/plans/UTISItem.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function UTISItem(props) {
   return (
-    <div className="utis-item">
-      <h2>{props.utis.name}</h2>
+    <div className="utis-item" key={props.utis.id}>
+      <h2 className="dark-purple-text" style={{ height: "80px" }}>
+        {props.utis.name}
+      </h2>
       <div className="utis-item-product">
-        <p>{props.utis.products.length} Productos</p>
+        <p>{props.utis.total_activities} Actividades</p>
       </div>
     </div>
   );
