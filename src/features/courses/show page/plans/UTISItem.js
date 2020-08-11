4fda4139562eee145576c961e9a966c4a1c833aa
kyoung90 <kenneth.young90@gmail.com>
2020-08-11 04:06:04 -0400

    -Add edit and delete btns -Make item size smaller

--- src/features/courses/show page/plans/UTISItem.js
+++ src/features/courses/show page/plans/UTISItem.js
@@ -4,22 +4,46 @@ import { withRouter } from "react-router-dom";
 
 function UTISItem(props) {
   const handleClick = () => {
-    props.history.push(`/cursos/${props.match.params.id}/utis/${props.utis.id}`);
+    props.history.push(
+      `/cursos/${props.match.params.id}/utis/${props.utis.id}`
+    );
   };
 
   return (
-    <div
-      className="utis-item"
-      key={props.utis.id}
-      onClick={() => {
-        handleClick();
-      }}
-    >
-      <h2 className="dark-purple-text" style={{ height: "80px" }}>
-        {props.utis.name}
-      </h2>
-      <div className="utis-item-product">
-        <p>{props.utis.total_activities} Actividades</p>
+    <div className="utis-item" key={props.utis.id}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "850px",
+        }}
+      >
+        <div
+          style={{ display: "flex" }}
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          <h2 className="dark-purple-text" style={{ height: "80px" }}>
+            {props.utis.name}
+          </h2>
+          <div className="utis-item-product">
+            <p>{props.utis.total_activities} Actividades</p>
+          </div>
+        </div>
+        <img
+          className="course-icon"
+          src="/assets/edit_icon.png"
+          alt="Edit UTIS"
+          onClick={() => props.generateModal(props.utis.id)}
+        />
+        <img
+          className="course-icon"
+          src="/assets/trash_icon.png"
+          alt="Delete UTIS"
+          onClick={() => props.generateDeleteModal(props.utis.id)}
+        />
       </div>
     </div>
   );
